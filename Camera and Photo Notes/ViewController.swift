60a85c7f3ac481003ad5c878981a5042a05789d8
Daniel Vega <daniel.vega23@d155.org>
2021-10-29 08:01:53 -0500

    finished the photo camera notes. Use for reference on gesture recognizer app

--- Camera and Photo Notes/ViewController.swift
+++ Camera and Photo Notes/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageViewOutlet: UIImageView!
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -16,10 +17,26 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imagePicker.delegate = self
     }
     @IBAction func photoAction(_ sender: UIButton) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     @IBAction func cameraAction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        }
+        else{
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.imageViewOutlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+    }
+    
+    
 }
 
